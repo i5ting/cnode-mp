4f6b41789efe02500b8a3815277f45e7e6a06688
i5ting <i5ting@126.com>
2016-11-25 11:25:38 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -4,12 +4,20 @@
  * Module dependencies.
  */
 
-var mkdirp = require('mkdirp');
+var fs = require('fs')
+var mkdirp = require('mkdirp')
 
-var dir = require('./src/dir')();
+var dir = require('./src/dir')()
     
 mkdirp(dir, function (err) {
     if (err) console.error(err)
     else console.log('pow!')
-});
+})
 
+var fileNames = ['topic', 'reply', 'opensource', 'news', 'other']
+
+for (var i = 0; i < fileNames.length; i++) {
+  var name = fileNames[i]
+  var _file_name = dir + '/' + name + '.md'
+  fs.writeFileSync(_file_name, name)
+}
